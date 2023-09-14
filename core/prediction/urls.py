2c95322100e64475a984ffdc3c6a3a46aeefaee7
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-14 23:48:37 +0000

    Update urls.py

--- core/prediction/urls.py
+++ core/prediction/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Overview.as_view(), name="overview")
+    path('', views.Overview.as_view(), name="overview"),
+    path('predict-outage/', views.PredictPowerOutage.as_view(), name="predict"),
 ]
