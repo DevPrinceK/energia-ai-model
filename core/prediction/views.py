bd022442baed82863e783025c434560636034f24
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-14 22:06:05 +0000

    Update views.py

--- core/prediction/views.py
+++ core/prediction/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from knox.views import LoginView as KnoxLoginView
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class Overview(APIView):
+    '''API Overview'''
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self, request, *args, **kwargs):
+        '''Returns the overview of the api - docs of the api'''
+        return Response({
+            "message": "Energia-AI API"
+        }, status=status.HTTP_200_OK)
