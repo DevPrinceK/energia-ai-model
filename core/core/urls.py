801b15aae4a16c71522a96a73324cb334c3145e7
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-14 23:48:11 +0000

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("prediction.urls")),
-    path("/", include("accounts.urls")),
+    path("api/", include("prediction.urls")),
+    path("account/", include("accounts.urls")),
 ]
